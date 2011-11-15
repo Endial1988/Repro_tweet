70677eae47b8aeefdd0ec7d03382ef04874425c1
Daniel Asberry <Endial1988@gmail.com>
2011-11-15 12:49:03 -0500

    Attempt at a Repro Twitter like Application for Learning Purposes.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ReproTweet::Application.config.secret_token = 'c79f3186f5721c352db72da9764c5220ef41eccd8804b72058b6a068de26682e12316bf1fc3c2754b9b2948a971e26b24092f2533777bc554929a0f48ca41713'
